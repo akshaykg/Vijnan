be1a9376677e976bd01f680a44e3af8218295c21
Akshay <akshaykg17@gmail.com>
2020-01-06 19:20:26 +0530

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,88 @@
+function New-AzServiceBusSasToken {  
+    param( 
+           
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Namespace,
+        [Parameter(Mandatory = $true)]
+        [string]
+        $PolicyName,
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Key
+    )
+
+    $origin = [DateTime]"1/1/1970 00:00" 
+    $Expiry = (Get-Date).AddMinutes(5)    
+
+    #compute the token expiration time.
+    $diff = New-TimeSpan -Start $origin -End $Expiry 
+    $tokenExpirationTime = [Convert]::ToInt32($diff.TotalSeconds)
+
+    #Create a new instance of the HMACSHA256 class and set the key to UTF8 for the size of $Key
+    $hmacsha = New-Object -TypeName System.Security.Cryptography.HMACSHA256
+    $hmacsha.Key = [Text.Encoding]::UTF8.GetBytes($Key)
+
+    #create the string that will be used when cumputing the hash
+    $stringToSign = [Web.HttpUtility]::UrlEncode($Namespace) + "`n" + $tokenExpirationTime
+
+    #Compute hash from the HMACSHA256 instance we created above using the size of the UTF8 string above.
+    $hash = $hmacsha.ComputeHash([Text.Encoding]::UTF8.GetBytes($stringToSign))
+    #Convert the hash to base 64 string
+    $signature = [Convert]::ToBase64String($hash)
+
+    #create the token
+    $token = [string]::Format([Globalization.CultureInfo]::InvariantCulture, `
+            "SharedAccessSignature sr={0}&sig={1}&se={2}&skn={3}", `
+            [Web.HttpUtility]::UrlEncode($Namespace), `
+            [Web.HttpUtility]::UrlEncode($signature), `
+            $tokenExpirationTime, $PolicyName) 
+    return $token
+}
+function Send-AzServiceBusMessage {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ResourceGroupName,
+        [Parameter(Mandatory = $true)]
+        [string]
+        $NamespaceName,
+        [Parameter(Mandatory = $true)]
+        [string]
+        $QueueName,    
+        [Parameter(Mandatory = $false)]
+        [string]
+        $PolicyName = 'RootManageSharedAccessKey'
+    )
+      
+    $message = [PSCustomObject] @{ "Body" = "new blob is added"; }
+    
+    $Namespace = (Get-AzServiceBusNamespace -ResourceGroupName $ResourceGroupName -Name $namespacename).Name
+    $key = (Get-AzServiceBusKey -ResourceGroupName $ResourceGroupName -Namespace $namespacename -Name $PolicyName).PrimaryKey
+    
+    $body = $Message.Body 
+    $Message.psobject.properties.Remove("Body")
+
+    $token = New-AzServiceBusSasToken -Namespace $Namespace -Policy $PolicyName -Key $Key
+    
+    #set up the parameters for the Invoke-WebRequest
+    $headers = @{ "Authorization" = "$token"; "Content-Type" = "application/atom+xml;type=entry;charset=utf-8" }
+    $uri = "https://$Namespace.servicebus.windows.net/$QueueName/messages"
+    $headers.Add("BrokerProperties", $(ConvertTo-Json -InputObject $Message -Compress))
+
+    #Invoke-WebRequest call.
+    #The normal output of the command is redirected to the $null automatic variable.
+    #If an error occurs, Invoke-WebRequest will output the error to the error stream stderr.
+    Invoke-WebRequest -Uri $uri -Headers $headers -Method Post -Body $body > $null
+}
+
+
+$serviceBussInstalled = Get-InstalledModule Az.ServiceBus
+if (!$serviceBussInstalled) {
+    #Install-Module Az.ServiceBus -Scope CurrentUser
+}
+
+Send-AzServiceBusMessage -ResourceGroupName India_rg2 `
+    -NamespaceName trrreeeee `
+    -QueueName test `
+    -PolicyName RootManageSharedAccessKey
